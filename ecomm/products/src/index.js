a6f52e5baa29a6bc24e8bb6d033d118c751a35c6
Maxim Klochkov <maxim.klochkov@gmail.com>
2022-11-09 00:23:59 +0300

    Product config done

--- ecomm/products/src/index.js
+++ ecomm/products/src/index.js
@@ -1,10 +1,14 @@
 import faker from 'faker'
 
+const parent = document.getElementById('dev-products')
+
 let products = ''
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
     const name = faker.commerce.productName()
-    products += `<div>${name}</div>`
+    products += `<div>${i+1}. ${name}</div>`
+
+    parent.innerHTML = products
 }
 
-console.log(products)
+// console.log(products)
